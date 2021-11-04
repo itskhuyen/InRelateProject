2bfcf0a32c408f19da4722592586bd249e13895f
Khuyen Nguyen <khuyennguyen@Khuyens-Air.lan>
2021-11-03 20:29:56 -0700

    inrelate with first function for total number of pairwise comparison

---
+++ Functions.For.Inrelate.R
@@ -0,0 +1,7 @@
+tot.Num.Of.Pairwise.Comparison<-function(n){
+  return(n*(n-1)/2)
+}
+
+#what is the input for the code? the output? functions in package?
+# the constant numbers in the code 
+#start with 3 tables but end with one table?
