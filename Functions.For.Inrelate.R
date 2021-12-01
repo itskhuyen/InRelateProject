e4971ea6151930bfe7fea2de7d159be266446d7b
Khuyen Nguyen <khuyennguyen@Khuyens-Air.lan>
2021-11-30 20:18:57 -0800

    inrelate v1.0

--- Functions.For.Inrelate.R
+++ Functions.For.Inrelate.R
@@ -2,6 +2,3 @@ tot.Num.Of.Pairwise.Comparison<-function(n){
   return(n*(n-1)/2)
 }
 
-#what is the input for the code? the output? functions in package?
-# the constant numbers in the code 
-#start with 3 tables but end with one table?
