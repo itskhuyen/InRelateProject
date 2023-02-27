5d1ac8a087a30bb25e6db8b51d58a53a140ef95e
Khuyen Nguyen <khuyennguyen@khuyens-air.illumina.com>
2023-02-27 10:50:53 -0800

    replaced code with functions

--- preliminary functions/preliminary functions.R
+++ preliminary functions/preliminary functions.R
@@ -152,7 +152,348 @@ calcCoeff <- function(IBS, l, indiv1a, indiv1.etaik, indiv2a, indiv2.etaik, locu
       d7 <- 0
       d8 <- 0
       d9 <- (zi1*zi1*zj2*zj2+zj1*zj1*zi2*zi2)/2
-    }}
+    }
+  if(IBS==3)	{
+    
+    pklallelei<-array(dim=c(K))
+    pklallelej<-array(dim=c(K))
+    
+    a<-indiv1a[l]
+    b<-indiv2b[l]
+    x<-which(locus1$V2==a[,])
+    y<-which(locus1$V2==b[,])
+    
+    allelei<-locus1[x,]
+    allelej<-locus1[y,]
+    
+    for(x in 1:K)	{
+      pklallelei[x]<-allelei[1,x+2]
+      pklallelej[x]<-allelej[1,x+2]
+    }
+    zi1=0
+    zi2=0
+    zj1=0
+    zj2=0
+    
+    for(x in 1:K)	{
+      zi1<-zi1+pklallelei[x]*indiv1.etaik[x]
+      zi2<-zi2+pklallelei[x]*indiv2.etaik[x]
+      zj1<-zj1+pklallelej[x]*indiv1.etaik[x]
+      zj2<-zj2+pklallelej[x]*indiv2.etaik[x]
+    }
+    
+    # Under S3
+    d1<-0
+    d2<-0
+    d3<-(zi1*zj2+zj1*zi2)/2
+    d4<-(zi1*zi2*zj2+zj1*zi2*zj2)/2
+    d5<-0
+    d6<-0
+    d7<-0
+    d8<-(zi1*zi2*zj2+zj1*zi2*zj2)/2
+    d9<-(zi1*zi1*zi2*zj2+zj1*zj1*zj2*zi2)/2
+    
+  }
+  
+  if(IBS==4)	{
+    
+    a<-indiv1a[l]
+    b<-indiv2a[l]
+    c<-indiv2b[l]
+    x<-which(locus1$V2==a[,])
+    y<-which(locus1$V2==b[,])
+    z<-which(locus1$V2==c[,])
+    
+    allelei<-locus1[x,]
+    allelej<-locus1[y,]
+    allelek<-locus1[z,]
+    
+    pklallelei<-array(dim=c(K))
+    pklallelej<-array(dim=c(K))
+    pklallelek<-array(dim=c(K))
+    
+    for(x in 1:K)	{
+      pklallelei[x]<-allelei[1,x+2]
+      pklallelej[x]<-allelej[1,x+2]
+      pklallelek[x]<-allelek[1,x+2]
+    }
+    
+    zi1=0
+    zi2=0
+    zj1=0
+    zj2=0
+    zk1=0
+    zk2=0
+    
+    for(x in 1:K)	{
+      zi1<-zi1+pklallelei[x]*indiv1.etaik[x]
+      zi2<-zi2+pklallelei[x]*indiv2.etaik[x]
+      zj1<-zj1+pklallelej[x]*indiv1.etaik[x]
+      zj2<-zj2+pklallelej[x]*indiv2.etaik[x]
+      zk1<-zk1+pklallelek[x]*indiv1.etaik[x]
+      zk2<-zk2+pklallelek[x]*indiv2.etaik[x]
+    }
+    
+    # Under S4
+    d1<-0
+    d2<-0
+    d3<-0
+    d4<-(zi1*zj2*zk2+zi2*zj1*zk1)/2
+    d5<-0
+    d6<-0
+    d7<-0
+    d8<-0
+    d9<-(zi1*zi1*zj2*zk2+zi2*zi2*zj1*zk1)/2
+  }
+  
+  if(IBS==5)	{
+    
+    a<-indiv2b[l]
+    b<-indiv1a[l]
+    x<-which(locus1$V2==a[,])
+    y<-which(locus1$V2==b[,])
+    
+    allelej<-locus1[x,]
+    allelei<-locus1[y,]
+    pklallelei<-array(dim=c(K))
+    pklallelej<-array(dim=c(K))
+    
+    for(x in 1:K)	{
+      pklallelei[x]<-allelei[1,x+2]
+      pklallelej[x]<-allelej[1,x+2]
+    }
+    
+    #need to define zi1,zi2,zj2,zj1
+    
+    zi1=0
+    zi2=0
+    zj1=0
+    zj2=0
+    
+    for(x in 1:K)	{
+      zi1<-zi1+pklallelei[x]*indiv1.etaik[x]
+      zi2<-zi2+pklallelei[x]*indiv2.etaik[x]
+      zj1<-zj1+pklallelej[x]*indiv1.etaik[x]
+      zj2<-zj2+pklallelej[x]*indiv2.etaik[x]
+    }
+    
+    # Now to get conditional probabilities:
+    #Under S5
+    d1<-0
+    d2<-0
+    d3<-0
+    d4<-0
+    d5<-(zi1*zj2+zj1*zi2)/2
+    d6<-(zi1*zi2*zj2+zj1*zi2*zj2)/2
+    d7<-0
+    d8<-(zi1*zi2*zj2+zj1*zi2*zj2)/2
+    d9<-(zi1*zi1*zi2*zj2+zj1*zj1*zj2*zi2)/2
+  }
+  
+  if(IBS==6)	{
+    
+    a<-indiv1a[l]
+    b<-indiv1b[l]
+    c<-indiv2b[l]
+    x<-which(locus1$V2==a[,])
+    y<-which(locus1$V2==b[,])
+    z<-which(locus1$V2==c[,])
+    
+    allelei<-locus1[z,]
+    allelej<-locus1[x,]
+    allelek<-locus1[y,]
+    
+    
+    pklallelei<-array(dim=c(K))
+    pklallelej<-array(dim=c(K))
+    pklallelek<-array(dim=c(K))
+    
+    for(x in 1:K)	{
+      pklallelei[x]<-allelei[1,x+2]
+      pklallelej[x]<-allelej[1,x+2]
+      pklallelek[x]<-allelek[1,x+2]
+    }
+    zi1=0
+    zi2=0
+    zj1=0
+    zj2=0
+    zk1=0
+    zk2=0
+    
+    for(x in 1:K)	{
+      zi1<-zi1+pklallelei[x]*indiv1.etaik[x]
+      zi2<-zi2+pklallelei[x]*indiv2.etaik[x]
+      zj1<-zj1+pklallelej[x]*indiv1.etaik[x]
+      zj2<-zj2+pklallelej[x]*indiv2.etaik[x]
+      zk1<-zk1+pklallelek[x]*indiv1.etaik[x]
+      zk2<-zk2+pklallelek[x]*indiv2.etaik[x]
+    }
+    
+    #Under S6
+    d1<-0
+    d2<-0
+    d3<-0
+    d4<-0
+    d5<-0
+    d6<-(zi1*zj2*zk2+zi2*zj1*zk1)/2
+    d7<-0
+    d8<-0
+    d9<-(zi1*zi1*zj2*zk2+zi2*zi2*zj1*zk1)/2
+    
+  }
+  
+  if(IBS==7) {
+    
+    a<-indiv1a[l]
+    b<-indiv1b[l]
+    
+    x<-which(locus1$V2==a[,])
+    y<-which(locus1$V2==b[,])
+    
+    allelei<-locus1[x,]
+    allelej<-locus1[y,]
+    pklallelei<-array(dim=c(K))
+    pklallelej<-array(dim=c(K))
+    
+    for(x in 1:K)	{
+      pklallelei[x]<-allelei[1,x+2]
+      pklallelej[x]<-allelej[1,x+2]
+    }
+    
+    zi1=0
+    zi2=0
+    zj1=0
+    zj2=0
+    
+    for(x in 1:K)	{
+      zi1<-zi1+pklallelei[x]*indiv1.etaik[x]
+      zi2<-zi2+pklallelei[x]*indiv2.etaik[x]
+      zj1<-zj1+pklallelej[x]*indiv1.etaik[x]
+      zj2<-zj2+pklallelej[x]*indiv2.etaik[x]
+    }
+    
+    #Under S7
+    d1<-0
+    d2<-0
+    d3<-0
+    d4<-0
+    d5<-0
+    d6<-0
+    d7<-(zi1*zj2+zj1*zi2)/2
+    d8<-(zj1*zj2*(zi1+zi2*0.5)+(zi1*zi2*(zj1+zj2)*0.5))/2
+    d9<-zi1*zi2*zj1*zj2
+  }
+  
+  if(IBS==8) {
+    a<-indiv1a[l]
+    b<-indiv1b[l]
+    c<-indiv2b[l]
+    
+    x<-which(locus1$V2==a[,])
+    y<-which(locus1$V2==b[,])
+    z<-which(locus1$V2==c[,])
+    
+    allelei<-locus1[x,]
+    allelej<-locus1[y,]
+    allelek<-locus1[z,]
+    
+    pklallelei<-array(dim=c(K))
+    pklallelej<-array(dim=c(K))
+    pklallelek<-array(dim=c(K))
+    
+    for(x in 1:K)	{
+      pklallelei[x]<-allelei[1,x+2]
+      pklallelej[x]<-allelej[1,x+2]
+      pklallelek[x]<-allelek[1,x+2]
+    }
+    
+    zi1=0
+    zi2=0
+    zj1=0
+    zj2=0
+    zk1=0
+    zk2=0
+    
+    for(x in 1:K)	{
+      zi1<-zi1+pklallelei[x]*indiv1.etaik[x]
+      zi2<-zi2+pklallelei[x]*indiv2.etaik[x]
+      zj1<-zj1+pklallelej[x]*indiv1.etaik[x]
+      zj2<-zj2+pklallelej[x]*indiv2.etaik[x]
+      zk1<-zk1+pklallelek[x]*indiv1.etaik[x]
+      zk2<-zk2+pklallelek[x]*indiv2.etaik[x]
+    }
+    
+    #Under S8
+    d1<-0
+    d2<-0
+    d3<-0
+    d4<-0
+    d5<-0
+    d6<-0
+    d7<-0
+    d8<-0.5*(((zi1+zi2)/2)*(zj1*zk2+zk1*zj2))
+    d9<-0.5*((zi1*zi2)*(zj1*zk2+zj2*zk1))
+  }
+  
+  if(IBS==9)	{
+    a<-indiv1a[l]
+    b<-indiv1b[l]
+    c<-indiv2a[l]
+    d<-indiv2b[l]
+    
+    x<-which(locus1$V2==a[,])
+    y<-which(locus1$V2==b[,])
+    z<-which(locus1$V2==c[,])
+    z1<-which(locus1$V2==d[,])
+    
+    allelei<-locus1[x,]
+    allelej<-locus1[y,]
+    allelek<-locus1[z,]
+    allelel<-locus1[z1,]
+    
+    pklallelei<-array(dim=c(K))
+    pklallelej<-array(dim=c(K))
+    pklallelek<-array(dim=c(K))
+    pklallelel<-array(dim=c(K))
+    
+    for(x in 1:K)	{
+      pklallelei[x]<-allelei[1,x+2]
+      pklallelej[x]<-allelej[1,x+2]
+      pklallelek[x]<-allelek[1,x+2]
+      pklallelel[x]<-allelel[1,x+2]
+    }
+    
+    zi1=0
+    zi2=0
+    zj1=0
+    zj2=0
+    zk1=0
+    zk2=0
+    zl1=0
+    zl2=0
+    
+    for(x in 1:K)	{
+      zi1<-zi1+pklallelei[x]*indiv1.etaik[x]
+      zi2<-zi2+pklallelei[x]*indiv2.etaik[x]
+      zj1<-zj1+pklallelej[x]*indiv1.etaik[x]
+      zj2<-zj2+pklallelej[x]*indiv2.etaik[x]
+      zk1<-zk1+pklallelek[x]*indiv1.etaik[x]
+      zk2<-zk2+pklallelek[x]*indiv2.etaik[x]
+      zl1<-zl1+pklallelel[x]*indiv1.etaik[x]
+      zl2<-zl2+pklallelel[x]*indiv2.etaik[x]
+    }
+    
+    #Under S9
+    d1<-0
+    d2<-0
+    d3<-0
+    d4<-0
+    d5<-0
+    d6<-0
+    d7<-0
+    d8<-0
+    d9<-(zi1*zj1*zk2*zl2+zk1*zl1*zi2*zj2)/2
+  }}
 
 #do optimization 
 doOptimization <- function(ibds) {
@@ -165,7 +506,7 @@ doOptimization <- function(ibds) {
 calcRelatedness1 <- function(mle) {
   2*(mle$pars[1]+0.5*(mle$pars[3]+mle$pars[5]+mle$pars[7])+0.25*(mle$pars[8]))}
 
-calcrRlatedness2 <- function(mle) {
+calcRelatedness2 <- function(mle) {
   2*(mle$pars[7]*0.5+0.25*mle$pars[8])}
     
     
